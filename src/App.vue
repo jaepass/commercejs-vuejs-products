7337f26ed5c18f2805d8a2587a6f9b78b999d4ea
Jaeriah Tay <jaeriahpasseri@gmail.com>
2020-09-20 23:34:35 -0700

    add merchant details function, add hero banner

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,42 @@
 <template>
-  <ProductsList :products="products"/>
+  <div>
+    <Hero :merchant="merchant" />
+    <ProductsList :products="products"/>
+  </div>
 </template>
 
 <script>
+import Hero from './components/Hero';
 import ProductsList from './components/ProductsList';
 
 export default {
   name: 'app',
   components: {
+    Hero,
     ProductsList,
   },
   data() {
     return {
+      merchant: {},
       products: [],
     }
   },
   created() {
+    this.fetchMerchantDetails();
     this.fetchProducts();
   },
   methods: {
+    /**
+     * Fetch merchant details
+     * https://commercejs.com/docs/api/?javascript--cjs#get-merchant-details
+     */
+    fetchMerchantDetails() {
+      this.$commerce.merchants.about().then((merchant) => {
+        this.merchant = merchant;
+      }).catch((error) => {
+        console.log('There was an error fetch the merchant details', error)
+      });
+    },
     /**
      * Fetch products data from Chec and stores in the products data object.
      * https://commercejs.com/docs/sdk/products
